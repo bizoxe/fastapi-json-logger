b28dc54d89b5d7215607e36add7c5a62dda469a8
alexander <alfacat187@gmail.com>
2024-12-27 11:46:53 +0300

    add in to lifespan queue handler

--- fastapi-application/create_fastapi_app.py
+++ fastapi-application/create_fastapi_app.py
@@ -1,16 +1,21 @@
 from contextlib import asynccontextmanager
+import logging
 
 from fastapi import FastAPI
 
 from utils.json_logger.setup import setup_logging
+from utils.json_logger.middlewares import LoggingMiddleware
 from core.config import settings
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     setup_logging()
+    queue_handler = logging.getHandlerByName("queue_handler")
+    queue_handler.listener.start()
 
     yield
+    queue_handler.listener.stop()
 
 
 def create_app() -> FastAPI:
@@ -19,5 +24,6 @@ def create_app() -> FastAPI:
         version=settings.api.version,
         lifespan=lifespan,
     )
+    app.middleware("http")(LoggingMiddleware())
 
     return app
