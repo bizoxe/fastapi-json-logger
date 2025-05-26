6542d9cb8d586de176912503aa7b0ef32bb7037c
alexander <alex.matveev.dev@gmail.com>
2025-05-26 20:27:10 +0300

    update public views

--- fastapi-application/api/api_v1/public/views.py
+++ fastapi-application/api/api_v1/public/views.py
@@ -1,11 +1,16 @@
+import logging
+
 from fastapi import APIRouter
 
 router = APIRouter(
     tags=["Public"],
 )
 
+logger = logging.getLogger(__name__)
+
 
 @router.get("")
-def greeting():
+async def greeting() -> dict[str, str]:
+    logger.info("Info message")
 
     return {"message": "Hello world from FastAPI app!"}
