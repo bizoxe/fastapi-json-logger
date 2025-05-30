766bab0e6a4658539fafc2dbede115127c7bd353
alexander <alex.matveev.dev@gmail.com>
2025-05-30 17:16:21 +0300

    add view func get_user_data

--- fastapi-application/api/api_v1/public/views.py
+++ fastapi-application/api/api_v1/public/views.py
@@ -1,6 +1,10 @@
 import logging
 
 from fastapi import APIRouter
+from pydantic import (
+    BaseModel,
+    EmailStr,
+)
 
 router = APIRouter(
     tags=["Public"],
@@ -9,8 +13,25 @@ router = APIRouter(
 logger = logging.getLogger(__name__)
 
 
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+
+
 @router.get("")
 async def greeting() -> dict[str, str]:
     logger.info("Info message")
 
     return {"message": "Hello world from FastAPI app!"}
+
+
+@router.post("/user")
+async def get_user_data(user: User) -> User:
+    logger.info(
+        "User data: first_name=%s, last_name=%s, email=%s",
+        user.first_name,
+        user.last_name,
+        user.email,
+    )
+    return user
